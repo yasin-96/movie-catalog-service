eab4c27a8cccfff270735d8feca5099a037fe1c1
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-05-03 14:55:28 +0200

    changed app.yml and added dependency in pom file

--- src/main/kotlin/io/eraslan/moviecatalogservice/controller/MovieCatalogController.kt
+++ src/main/kotlin/io/eraslan/moviecatalogservice/controller/MovieCatalogController.kt
@@ -9,7 +9,6 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/catalog")
 class MovieCatalogController(
     private val movieCatalogService: MovieCatalogService
 ) {
